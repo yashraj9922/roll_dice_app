4d67016e1483a3dbae21376a6cfa3f6959904446
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-08-13 03:21:27 +0530

    linear gradient added, using decoration widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 
-
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget{
-  Widget build(BuildContext context){
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+  runApp(
+    MaterialApp(
+      // const keyword help Dart to optimize runtime performance
+      //when same widget with same data is used for multiple time in an app...it will use sam existing memory location
+      // Avoiding duplication of memory hence making App more efficicent and fast
+      home: Scaffold(
+        // backgroundColor: Color.fromARGB(255, 250, 209, 57),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: Colors.accents),
+          ),
+          child: const Center(
+            child: Text("HELLO WORLD"),
+          ),
+        ),
       ),
-    );
-  }
+    ),
+  );
 }
